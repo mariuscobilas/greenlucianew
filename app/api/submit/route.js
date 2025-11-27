5ec9ba0ba6e331fc4a83faf3b7fb1e5959ff87d9
Cobîlaș Marius <cobilasmarius@gmail.com>
2025-11-27 21:14:36 +0200

    V1.2.2 small changes

--- app/api/submit/route.js
+++ app/api/submit/route.js
@@ -8,28 +8,24 @@ export async function POST(req) {
         //Config
         const config = {
             server: "https://cloud.seatable.io",
-            APIToken: process.env.SEATABLE_API_KEY, // Replace with your generic APIToken
+            APIToken: process.env.SEATABLE_API_KEY,
         };
 
         const base = new Base(config);
         await base.auth();
 
-        //Creating new row
+        //Adaugarea newRow
         const newRow = {
             "FirstName": formData.firstName,
             "LastName": formData.lastName,
             "Email": formData.email,
             "Organisation": formData.organisation,
-            "Status": "New",                 // Single Select column
-            "Submission Date": new Date().toISOString() // Date column
+            "Status": "New",
+            "Submission Date": new Date().toISOString()
         };
 
-
-
-        // 4. Append the row
         const result = await base.appendRow(tableName, newRow);
 
-        // 5. Success Response
         return Response.json({
             status: "success",
             message: "Row created successfully",
