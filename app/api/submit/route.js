4f6cdfe34a9099d5a936c46bc92d74d68a3aaa21
Cobîlaș Marius <cobilasmarius@gmail.com>
2025-11-30 14:09:20 +0200

    V2 DONE PROD Refactored the sending to seatable function.

--- app/api/submit/route.js
+++ app/api/submit/route.js
@@ -1,20 +1,22 @@
-import { Base } from "seatable-api";
-
 export async function POST(req) {
     try {
         const formData = await req.json();
-        const tableName = "Table1";
 
-        //Config
-        const config = {
-            server: "https://cloud.seatable.io",
-            APIToken: process.env.SEATABLE_API_KEY,
+        // Get Bearer
+        const getTokenUrl = 'https://cloud.seatable.io/api/v2.1/dtable/app-access-token/?exp=1h';
+        const getTokenOptions = {
+            method: 'GET',
+            headers: {
+                'accept': 'application/json',
+                'authorization': `Bearer ${process.env.SEATABLE_API_KEY}`
+            }
         };
 
-        const base = new Base(config);
-        await base.auth();
+        const responseGetToken = await fetch(getTokenUrl, getTokenOptions);
+        const tokenData = await responseGetToken.json();
+        const accessToken = tokenData.access_token;
 
-        //Adaugarea newRow
+        // Add new Row
         const newRow = {
             "FirstName": formData.firstName,
             "LastName": formData.lastName,
@@ -24,16 +26,33 @@ export async function POST(req) {
             "Submission Date": new Date().toISOString()
         };
 
-        const result = await base.appendRow(tableName, newRow);
+        const addRowUrl = 'https://cloud.seatable.io/api-gateway/api/v2/dtables/308d588a-b7ef-44ed-8edb-ad0ac10f9ad2/rows/';
+        const addRowOptions = {
+            method: 'POST',
+            headers: {
+                'accept': 'application/json',
+                'content-type': 'application/json',
+                'authorization': `Bearer ${accessToken}`
+            },
+            body: JSON.stringify({
+                rows: [newRow],
+                table_name: 'Table1'
+            }),
+        };
+
+        const addRowResponse = await fetch(addRowUrl, addRowOptions);
+        const addRowData = await addRowResponse.json();
+
+        console.log("Row added:", addRowData);
 
         return Response.json({
             status: "success",
             message: "Row created successfully",
-            row_id: result._id
+            data: addRowData
         }, { status: 200 });
 
     } catch (error) {
-        console.error("SeaTable Error:", error);
+        console.error("SeaTable Error:", error.message);
         return Response.json({
             status: "error",
             message: error.message
